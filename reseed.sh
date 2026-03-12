eb78b144c79df33e56caca68ada1efda5893a93e
krishnaTO <krishna.udaiwal@gmail.com>
2026-03-12 12:55:24 -0400

    added all components

--- reseed.sh
+++ reseed.sh
@@ -4,8 +4,8 @@ sh seed-via-docker.sh --clean -C project.yaml
 rm -rf target/immunotest/src/sparql
 cp -r sparql target/immunotest/src/sparql
 
-for id in chr cl ec go hgnc hgnc.genegroup mgi nbo ncbitaxon pato pr rgd uberon uniprot; do
-    rm target/immunotest/src/ontology/imports/${id}_terms.txt
+for id in chr cl ec go hgnc mgi nbo ncbitaxon pato pr rgd uberon uniprot; do
+    #rm target/immunotest/src/ontology/imports/${id}_terms.txt
     cp imports/${id}_terms.txt target/immunotest/src/ontology/imports/${id}_terms.txt
 done
 
