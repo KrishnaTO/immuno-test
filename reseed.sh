09ea45a12a6181eeb8ff07af557fe1a0a577da1c
Krishna Udaiwal <krishna.udaiwal@gmail.com>
2026-03-16 13:24:51 -0400

    change mirror for hgnc remove cp of sparql queries

--- reseed.sh
+++ reseed.sh
@@ -2,7 +2,7 @@
 
 sh seed-via-docker.sh --clean -C project.yaml
 rm -rf target/immunotest/src/sparql
-cp -r sparql target/immunotest/src/sparql
+#cp -r sparql target/immunotest/src/sparql
 
 for id in chr cl ec go hgnc mgi nbo ncbitaxon pato pr rgd uberon uniprot; do
     #rm target/immunotest/src/ontology/imports/${id}_terms.txt
